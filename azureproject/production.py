38326a3f65c1603fbeb8e764eed74d5f9e9ae8b7
kauer3 <kaue0607@gmail.com>
2023-09-20 07:43:25 +0000

    Initial commit

--- azureproject/production.py
+++ azureproject/production.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
 
